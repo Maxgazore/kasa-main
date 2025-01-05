92907e132f6280555cc9850fcab451e4e043698b
Maxgazore <167684044+Maxgazore@users.noreply.github.com>
2025-01-05 21:07:17 +0100

    Refactoring de la page "A propos".

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,28 @@
 import { useState } from 'react'
 
 function About() {
+  const data = [
+    {
+      title: 'Fiabilité',
+      text: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont comformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+    },
+    {
+      title: 'Respect',
+      text: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme.',
+    },
+    {
+      title: 'Service',
+      text: 'La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.',
+    },
+    {
+      title: 'Sécurité',
+      text: 'La securité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l&apos;hôte qu&apos;au locataire, cela permet à nos équipes de vérifier que les stantards sont bien respecté. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.',
+    },
+  ]
+
   const [selected, setSelected] = useState(null)
 
-  const toggle = (i) => {
+  function toggle(i) {
     if (selected == i) {
       return setSelected(null)
     }
@@ -18,90 +37,26 @@ function About() {
         alt="Image d'un paysage"
       />
       <div className="about__info">
-        <div className="about__info__part">
-          <div className="about__info__part__collapser">
-            <h2>Fiabilité</h2>
-            <i
-              className="fa-solid fa-chevron-up fa-xl"
-              onClick={() => toggle(1)}
-            ></i>
+        {data.map((item, i) => (
+          <div key={item.title} className="about__info__part">
+            <div className="about__info__part__collapser">
+              <h2>{item.title}</h2>
+              <i
+                className="fa-solid fa-chevron-up fa-xl"
+                onClick={() => toggle(i)}
+              ></i>
+            </div>
+            <p
+              className={
+                selected == i
+                  ? 'about__info__part__show'
+                  : 'about__info__part__hide'
+              }
+            >
+              {item.text}
+            </p>
           </div>
-          <p
-            className={
-              selected == 1
-                ? 'about__info__part__show'
-                : 'about__info__part__hide'
-            }
-          >
-            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-            photos sont comformes aux logements, et toutes les informations sont
-            régulièrement vérifiées par nos équipes.
-          </p>
-        </div>
-        <div className="about__info__part">
-          <div className="about__info__part__collapser">
-            <h2>Respect</h2>
-            <i
-              className="fa-solid fa-chevron-up fa-xl"
-              onClick={() => toggle(2)}
-            ></i>
-          </div>
-          <p
-            className={
-              selected == 2
-                ? 'about__info__part__show'
-                : 'about__info__part__hide'
-            }
-          >
-            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-            comportement discriminatoire ou de pertubation du voisinage
-            entraînera une exclusion de notre plateforme.
-          </p>
-        </div>
-        <div className="about__info__part">
-          <div className="about__info__part__collapser">
-            <h2>Service</h2>
-            <i
-              className="fa-solid fa-chevron-up fa-xl"
-              onClick={() => toggle(3)}
-            ></i>
-          </div>
-          <p
-            className={
-              selected == 3
-                ? 'about__info__part__show'
-                : 'about__info__part__hide'
-            }
-          >
-            La qualité du service est au cœur de notre engagement chez Kasa.
-            Nous veillons à ce que chaque interaction, que ce soit avec nos
-            hôtes ou nos locataires, soit empreinte de respect et de
-            bienveillance.
-          </p>
-        </div>
-        <div className="about__info__part">
-          <div className="about__info__part__collapser">
-            <h2>Sécurité</h2>
-            <i
-              className="fa-solid fa-chevron-up fa-xl"
-              onClick={() => toggle(4)}
-            ></i>
-          </div>
-          <p
-            className={
-              selected == 4
-                ? 'about__info__part__show'
-                : 'about__info__part__hide'
-            }
-          >
-            La securité est la priorité de Kasa. Aussi bien pour nos hôtes que
-            pour les voyageurs, chaque logement correspond aux critères de
-            sécurité établis par nos services. En laissant une note aussi bien à
-            l&apos;hôte qu&apos;au locataire, cela permet à nos équipes de
-            vérifier que les stantards sont bien respecté. Nous organisons
-            également des ateliers sur la sécurité domestique pour nos hôtes.
-          </p>
-        </div>
+        ))}
       </div>
     </section>
   )
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,10 +5,10 @@ function Error() {
     <section className="error">
       <h1 className="error__title">404</h1>
       <h2 className="error__subtitle">
-        Oups! La page que vous demandez n'existe pas.
+        Oups! La page que vous demandez n&apos;existe pas.
       </h2>
       <Link className="error__link" to="/">
-        Retourner sur la page d’accueil
+        Retourner sur la page d&apos;accueil
       </Link>
     </section>
   )
